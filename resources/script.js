a4250d4a2146a2dfd4b5d2b9abbebceaae23b4fc
roctalvarado <roctalvarado@gmail.com>
2025-09-23 11:30:14 -0700

    Test

--- resources/script.js
+++ resources/script.js
@@ -1,1 +1,16 @@
-document.addEventListener
+// DOM -> Document Object Model
+// Evento que se dispara cuando se termina de cargal el DOM
+document.addEventListener("DOMContentLoaded", function(){
+
+    // Declaramos una variable en JavaScript (var) que representa el botón "btn-ok" en el archivo HTML
+    var btnOK = document.getElementById("btn-ok");
+    // Declaramos un evento click a ese botón
+    btnOK.addEventListener("click", function() {
+
+        // Declaramos una variable que representa el input del HTML
+        var name = document.getElementById("input-name");
+
+        // Mostramos una alerta del navegador con el contenido del input
+        alert("Hello World " + name.value);
+    })
+})
